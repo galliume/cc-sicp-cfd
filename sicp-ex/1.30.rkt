a3c58bc76a7fe190ead461cdaaed0d996dd9a910
galliume <gcconantc@gmail.com>
2025-12-06 23:52:53 +0100

    1.30

--- sicp-ex/1.30.rkt
+++ sicp-ex/1.30.rkt
@@ -3,17 +3,14 @@
 (require racket/trace)
 
 (define (sum term a next b)
-  (if (> a b)
-      0
-      (+ (term a)
-         (sum term (next  a) next b))))
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
 
 (define (cube a)
   (* a a a))
-
-(define (integral f a b dx)
-  (define (add-dx x) (+ x dx))
-  (* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
 
 (define (even? n)
   (= (remainder n 2) 0))
@@ -34,9 +31,6 @@
     (+ k 1))
   
   (* (/ h 3.0) (sum term 0 next n)))
-
-(integral cube 0 1 0.01)
-(integral cube 0 1 0.001)
 
 (simpson cube 0 1 100)
 (trace sum)
